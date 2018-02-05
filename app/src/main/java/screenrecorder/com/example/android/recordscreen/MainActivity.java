aee158e11de308bc9b1d3254235175b2dfc866c4
Unknown <Zachary.Lynch1337@gmail.com>
2018-02-04 17:34:46 -0800

    Added a couple todo's for now.

--- app/src/main/java/screenrecorder/com/example/android/recordscreen/MainActivity.java
+++ app/src/main/java/screenrecorder/com/example/android/recordscreen/MainActivity.java
@@ -1,13 +1,69 @@
 package screenrecorder.com.example.android.recordscreen;
-
+// TODO 1. Import all necessary libraries
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.MediaRecorder;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.media.projection.*;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
+    // TODO 2. Add variables for the following: mScreenDensity, display width+height
+    // TODO 3. The button, media projection, virtualdisplay, media recorder,
+    // TODO 3. request permission, and isRecording
+
+    // all variables
+    private static final int displayWidth = 1080;
+    private static final int displayHeight = 1920;
+    private static final int REQUEST_CODE = 1000;
+    private int screenDensity;
+    Button btn_action;
+    private VirtualDisplay  mVirtualDisplay;
+    private MediaProjection  mMediaProjection;
+    private MediaRecorder mMediaRecorder;
+    private static int pemissionKey = 1;
+    boolean isRecording = false;
+    // TODO 5. add static method for the orientation of screen. format:  ORIENTATION.append(Surface.ROTATION_0,90)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // TODO 6. String array that holds the PERMISSIONS{ Manifest.permission.WRITE_INTERNAL_STORAGE,}
+
+        // TODO 7. If statement to check if the permissions are granted and request the permissionKey
+
+        // TODO 8. Set up DisplayMatrix and initialize mediarecorder method
+
+
+
+
+        // TODO 9. Get id for button and check for onClick actions
     }
+
+    // TODO 10. Make method to change text in button if the recorder is recording or not
+
+
+    // TODO 11. method to toggle the screen sharing
 }
