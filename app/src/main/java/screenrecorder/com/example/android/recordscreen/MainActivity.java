704ee42f0c393793e87bd1add317f7826b388648
Unknown <Zachary.Lynch1337@gmail.com>
2018-02-04 16:24:43 -0800

    Initial commit

---
+++ app/src/main/java/screenrecorder/com/example/android/recordscreen/MainActivity.java
@@ -0,0 +1,13 @@
+package screenrecorder.com.example.android.recordscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
